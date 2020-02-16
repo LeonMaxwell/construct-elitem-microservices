5a4ad706f5cd8c9040ce4f10a0deb329729e8059
LeonMaxwell <java.leon@mail.ru>
2020-02-16 12:27:52 +0300

    First fixed commit my library

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='construct-elitem-microservices',
+    version='0.0.1',
+    packages=['construct_elitem_microservices'],
+    url='',
+    license='MIT',
+    author='Ryslan Boiko',
+    author_email='leonboiko061@gmail.com',
+    description='This module needs for make microservices elitem assistents'
+)
